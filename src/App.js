44a27e88504f07e4c2514cdb4af97057129fef40
Azimjon1111 <azimjon.nodir@gmail.com>
2022-11-07 15:34:35 +0500

    inital c0mmit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-red-600 text-lg'>Done</p>
+    </div>
+  );
+}
+
+export default App;
